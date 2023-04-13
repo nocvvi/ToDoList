cc34c1b7a706a5810fd9a344f76fadb6221957ba
Дмитрий <dima-vrn00@inbox.ru>
2023-04-13 23:26:07 +0300

    Initial Commit

---
+++ ToDoList/Model.swift
@@ -0,0 +1,74 @@
+//
+//  Model.swift
+//  ToDoList
+//
+//  Created by Дмитрий Aлёхин on 13.04.2023.
+//
+
+import Foundation
+import UserNotifications
+import UIKit
+
+var ToDoItems: [[String: Any]] {
+    set{
+        UserDefaults.standard.set(newValue, forKey: "ToDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    get{
+        if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String: Any]]{
+            return array
+        }else{
+            return  []
+        }
+        
+    }
+}
+
+func addItem (nameItem: String, isCompleted: Bool = false) {
+    ToDoItems.append(["Name": nameItem, "isCompleted": false])
+    setBadge()
+}
+ 
+func removeItem (at index: Int){
+    ToDoItems.remove(at: index)
+    setBadge()
+}
+
+func moveItem(fromIndex: Int, toIndex: Int){
+    
+    let from = ToDoItems[fromIndex]
+    ToDoItems.remove(at: fromIndex)
+    ToDoItems.insert(from, at: toIndex)
+}
+
+func changeState(at item: Int) -> Bool{
+    ToDoItems[item]["isCompleted"] =  !(ToDoItems[item]["isCompleted"] as! Bool)
+    setBadge()
+    return ToDoItems[item]["isCompleted"] as! Bool
+    
+}
+
+func requestForNotification(){
+    UNUserNotificationCenter.current().requestAuthorization(options: [.badge]) { isEnabled, error in
+        /*if isEnabled{
+            
+        }else{
+            
+        }
+         */
+    }
+}
+
+
+
+func setBadge(){
+    var totalBadgeNumber = 0
+    for item in ToDoItems {
+        if (item ["isCompleted"] as? Bool) == false{
+            totalBadgeNumber = totalBadgeNumber + 1
+    }
+    }
+    
+    UIApplication.shared.applicationIconBadgeNumber = totalBadgeNumber
+    
+}
---
+++ ToDoList/TableViewController.swift
@@ -0,0 +1,174 @@
+//
+//  TableViewController.swift
+//  ToDoList
+//
+//  Created by Дмитрий Aлёхин on 13.04.2023.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    @IBAction func pushEditAction(_ sender: Any) {
+        tableView.setEditing(!tableView.isEditing, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
+            self.tableView.reloadData()
+        }
+        //
+    }
+    
+    
+    @IBAction func pushAddAction(_ sender: Any) {
+        let alertController = UIAlertController(title: "Create new item", message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField { (textFiled) in
+            textFiled.placeholder = "New item name"
+        }
+        
+        let alertAction1 = UIAlertAction(title: "Cancel", style: .destructive) { (alert) in
+        }
+        
+        let alertAction2 = UIAlertAction(title: "Create", style: .cancel) { (alert) in
+             //добавить новую запись
+           let newItem = alertController.textFields![0].text
+            if newItem != ""{
+                addItem(nameItem: newItem!)
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+        alertController.addAction(alertAction1)
+        alertController.addAction(alertAction2)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = UIColor.systemGroupedBackground
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return ToDoItems.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let currentItem = ToDoItems[indexPath.row]
+
+        cell.textLabel?.text = currentItem["Name"] as? String
+        
+        if (currentItem["isCompleted"] as? Bool) == true{
+            cell.imageView?.image = UIImage(named: "check.png")
+        } else {
+            cell.imageView?.image = UIImage(named: "uncheck.png")
+        }
+        
+        if tableView.isEditing{
+            cell.textLabel?.alpha = 0.4
+            cell.imageView?.alpha = 0.4
+        }else{
+            cell.textLabel?.alpha = 1
+            cell.imageView?.alpha = 1
+            
+        }
+        
+        return cell
+    }
+
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            removeItem(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath , animated: true)
+        
+        if changeState(at: indexPath.row) {
+            tableView.cellForRow(at: indexPath)?.imageView?.image = UIImage(named: "check.png")
+        } else{
+            tableView.cellForRow(at: indexPath)?.imageView?.image = UIImage(named: "uncheck.png")
+        }
+       
+        
+        
+        
+    }
+    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+        moveItem(fromIndex: fromIndexPath.row, toIndex: to.row)
+    
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if tableView.isEditing{
+            return .none
+        }else{
+            return .delete
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
